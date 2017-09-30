e77f80c9af0f30400f16ac71b0d9b1f14e91518d
John Whittenburg <jawhittenburg@gmail.com>
2017-09-30 11:21:04 -0500

    adding form to navbar to search for characters

--- app/index.js
+++ app/index.js
@@ -9,38 +9,50 @@ import 'whatwg-fetch';
 //   <img class="img-container__img" src="http://worldversus.com/img/ironman.jpg" alt="">
 // `;
 
-// const myHeaders = new Headers();
-// const myInit = {
-//   method: 'GET',
-//   headers: myHeaders,
-//   mode: 'no-cors',
-//   cache: 'default'
-// };
-
 const pubKey = 'apikey=7a18a6993fb085a7db47c5326aa3ba70';
 const id = 109368;
+
+const fetchCharacterFocus = function (url) {
+  fetch(`${url}?${pubKey}`)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data)
+  });
+};
+
+const fetchArray = function (url) {
+  fetch(`${url}?${pubKey}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.data.results)
+    });
+};
+
+fetchCharacterFocus()
+fetchArray('https://gateway.marvel.com:443/v1/public/characters');
 
 
 fetch(`https://gateway.marvel.com:443/v1/public/characters?${pubKey}`)
   .then((res) => res.json())
   .then((data) => {
-    console.log(data.data.results[0]);
-
     const characterList = data.data.results;
-    characterList.forEach(function (item) {
-      // console.log(`${item.thumbnail.path}/landscape_xlarge.${item.thumbnail.extension}`);
+    characterList.forEach((character) => {
+// API Image Path - character.thumbnail.path + picture_size + character.thumbnail.extension
 
       const ul = document.querySelector('.section__ul');
       const liItem = document.createElement('li');
       liItem.classList.add('li-item');
       ul.appendChild(liItem).innerHTML = `
         <div class="li-item__img-box">
-          <img src="${item.thumbnail.path}/standard_fantastic.${item.thumbnail.extension}" alt="" class="li-item__img-box__img" />
+          <img src="${character.thumbnail.path}/standard_fantastic.${character.thumbnail.extension}" alt="" class="li-item__img-box__img" />
         </div>
         <div class="li-item__text-box">
-          <h3 class="li-item__text-box__name">${item.name}</h3>
+          <h3 class="li-item__text-box__name">${character.name}</h3>
         </div>
       `;
+      document.querySelector('.li-item__text-box__name').addEventListener('click', () => {
+        console.log(character.urls[2].url);
+      });
     });
   });
 
