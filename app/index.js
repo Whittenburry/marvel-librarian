603dc35b71e0cca9dd4ccf093c9fbbcb838c6fc0
John Whittenburg <jawhittenburg@gmail.com>
2017-10-02 01:12:20 -0500

    updates to styling; now rendering fetched objects

--- app/index.js
+++ app/index.js
@@ -2,15 +2,10 @@
 
 import 'whatwg-fetch';
 
-// Going to need a media query stacking column__primary on top of column__secondary
-// const imgBox = document.querySelector('.img-container');
-//
-// imgBox.innerHTML = `
-//   <img class="img-container__img" src="http://worldversus.com/img/ironman.jpg" alt="">
-// `;
+// Prevent Default 'Sumbit' on Form | .addEventListener(`submit`, (ev) => { ev.preventDefault(); });
+document.querySelector('.form').addEventListener('submit', (ev) => { ev.preventDefault(); });
 
 const pubKey = 'apikey=7a18a6993fb085a7db47c5326aa3ba70';
-const id = 109368;
 
 const fetchCharacterFocus = function (url) {
   fetch(`${url}?${pubKey}`)
@@ -55,21 +50,3 @@ fetch(`https://gateway.marvel.com:443/v1/public/characters?${pubKey}`)
       });
     });
   });
-
-
-// creating basic subject-view; looping over html wireframe
-  //
-  // const ul = document.querySelector('.section__ul');
-  //
-  // for (let i = 1; i <= 20; i++) {
-  //   const liItem = document.createElement('li');
-  //   liItem.classList.add('li-item');
-  //   ul.appendChild(liItem).innerHTML = `
-  //     <div class="li-item__img-box">
-  //       <img src="https://static-cdn.jtvnw.net/jtv_user_pictures/ironmangoat-profile_image-49d896b9b298eb99-300x300.jpeg" alt="" class="li-item__img-box__img">
-  //     </div>
-  //     <div class="li-item__text-box">
-  //       <h3 class="li-item__text-box__name">Character #${i}</h3>
-  //     </div>
-  //   `;
-  // }
